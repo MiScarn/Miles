7af6f5544e3707aabed9ec364e1c764453008ef2
MiScarn <bringmenoize@gmail.com>
2023-02-03 23:46:41 +0600

    miles per rubles

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+ public class Main {
+
+     public static void main(String[] args) {
+         int amount = 20_000;
+
+         int rublesPerMile = 20;
+
+         int miles = amount / rublesPerMile;
+
+         System.out.println(miles);
+     }
+ }
